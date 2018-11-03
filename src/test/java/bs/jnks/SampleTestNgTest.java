ae1a9200b35b62304dcab6dafdca66daec154d5d
Yury <yakuramori@gmail.com>
2018-11-03 19:34:42 +0000

    removed useless classes.

--- src/test/java/bs/jnks/SampleTestNgTest.java
+++ src/test/java/bs/jnks/SampleTestNgTest.java
@@ -1,23 +1,14 @@
 package bs.jnks;
 
-import bs.jnks.pages.HomePage;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SampleTestNgTest extends TestNgTestBase {
 
-    private HomePage homepage;
-
-    @BeforeMethod
-    public void initPageObjects() {
-        homepage = PageFactory.initElements(driver, HomePage.class);
-    }
-
     @Test
     public void testHomePageHasAHeader() {
         driver.get(baseUrl);
-        Assert.assertFalse("".equals(homepage.header.getText()));
+        Assert.assertFalse("".equals(driver.findElement(By.xpath("//h1")).getText()));
     }
 }
